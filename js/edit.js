48d9c6a1b10cf7f998ff327ac3e2a42c6692dd69
Kuro666 <kuro2312@gmail.com>
2016-05-03 21:08:58 +0700

    Content: Add Combobox for Selecting Nations and City/State

--- js/edit.js
+++ js/edit.js
@@ -1,7 +1,41 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
-	$scope.aglProfile = "images/profile1.png";
 	
+	 $http({
+    method : "GET",
+    url : "js/data.json"
+  }).then(function mySucces(response) {
+		$scope.aglFullName = response.data.FullName;
+
+		$scope.aglJob = response.data.Job;
+		$scope.aglPhone = response.data.Phone;
+		$scope.aglEmail = response.data.Email;
+		$scope.aglAddress = response.data.Address;
+		$scope.aglNation = response.data.Nation;
+		$scope.aglMoreDetailAboutAddress = response.data.MoreDetailAboutAddress;
+		$scope.aglProfile = "images/profile1.png";
+		$scope.aglProfile = response.data.ProfileImage;
+		
+		$scope.aglHeaders = response.data.Headers;
+		
+		$scope.aglSummary = response.data.Summary;
+
+		$scope.aglEducation = response.data.Education;
+
+		$scope.aglSkills = response.data.Skills;
+
+		$scope.aglInterests = response.data.Interests;
+
+		$scope.aglLanguages = response.data.Languages;
+
+		$scope.aglProjects = response.data.Projects;
+		
+    }, function myError(response) {
+      $scope.aglFullName = response.statusText;
+  });
+  	
+	populateCountries("country", "state");
+
     $scope.imageUpload = function(element){
         var reader = new FileReader();
         reader.onload = $scope.imageIsLoaded;
