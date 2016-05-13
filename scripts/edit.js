19041d0a5eab3d3578e07cf90574fc2520f9e4f4
Kuro666 <kuro2312@gmail.com>
2016-05-13 08:28:14 +0700

    Content: Must Login to view/edit web
    Details:
    1/ If custom dont login, automatically return login page.

--- scripts/edit.js
+++ scripts/edit.js
@@ -1,10 +1,40 @@
 var app = angular.module("myApp", ["firebase"]);
 
-app.controller("myCtrl", ["$scope", "$firebaseObject",
-	function($scope, $firebaseObject) {
+app.factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) {
+    var ref = new Firebase("https://cv-1312094.firebaseio.com");
+    return $firebaseAuth(ref);
+  }
+]);
+
+app.controller("myCtrl", ["$scope", "$firebaseObject", "Auth",
+	function($scope, $firebaseObject, Auth) {
 	  
+		$scope.CheckLogin = function()
+		{
+			var user = Auth.$getAuth();
+						
+			if (user == null)
+				window.location.href = "login.html";
+			
+			//alert("Authenticated user with uid:" + user.uid);
+		}
+		
+		$scope.LogOut = function()
+		{
+			Auth.$unauth();
+			
+			var user = Auth.$getAuth();
+						
+			if (user == null)
+				window.location.href = "login.html";
+		}
+		
 		$scope.initData = function()
 		{
+			// Kiểm tra có đăng nhập không
+			$scope.CheckLogin();
+			
 			// Lấy dữ liệu của user
 			var ref = new Firebase("https://cv-1312094.firebaseio.com/web/saving-data/fireblog/users");	
 			$scope.aglUser = $firebaseObject(ref);
